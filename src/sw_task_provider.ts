a34f79bcfe6a540fdc48630154ac26336d49c522
Egor Pugin <egor.pugin@gmail.com>
2021-12-25 14:13:26 +0300

    Add a lot of invoke options.

--- src/sw_task_provider.ts
+++ src/sw_task_provider.ts
@@ -34,6 +34,12 @@ function exists(file: string): Promise<boolean> {
 
 interface SwTaskDefinition extends vscode.TaskDefinition {
 	task: string;
+
+	action: string; // build, generate, configure, run explan etc.
+	compiler: string;
+	configuration: string;
+	libraries: string; // static, shared
+	buildCurrentFile: boolean;
 }
 
 async function getSwTasks(): Promise<vscode.Task[]> {
@@ -47,52 +53,112 @@ async function getSwTasks(): Promise<vscode.Task[]> {
 		if (!folderString) {
 			continue;
 		}
-		const swFile = path.join(folderString, 'sw.cpp');
-		if (!await exists(swFile)) {
-			continue;
+
+		//
+		{
+			const taskName = "build current file (last configuration)";
+			const kind: SwTaskDefinition = {
+				type: 'sw',
+				task: taskName,
+				action: "build",
+				compiler: "",
+				configuration: "",
+				libraries: "",
+				buildCurrentFile: true
+			};
+			var cmd = "sw build -sfc -el --file ${file}";
+			const task = new vscode.Task(kind, workspaceFolder, taskName, 'sw', new vscode.ShellExecution(cmd), "$gcc");
+			result.push(task);
+			task.group = vscode.TaskGroup.Build;
 		}
 
+		//
         const runExplan = "run execution plan";
 		const compilerType = ["gcc", "clang"/*, "msvc"*/];
         const actionType = ["build", "generate", "configure", runExplan, "test"];
         const configurationType = [["debug","d"], ["release","r"], ["release with debug info","rwdi"]]; // msr
         const staticShared = ["static", "shared"];
         for (const compiler of compilerType) {
-			for (const action of actionType) {
-				for (const configuration of configurationType) {
-					for (const stsh of staticShared) {
-						var taskName = action + " " + compiler + " " + stsh + " " + configuration[0];
+			for (const configuration of configurationType) {
+				for (const stsh of staticShared) {
+					var build_cmd;
+					var generate_cmd;
+					var configure_cmd;
+					for (const action of actionType) {
+						var addBuildCurrentFileOption = [false];
 						if (action === runExplan) {
-							taskName += " (call after configure)";
+							addBuildCurrentFileOption = [true, false];
 						}
-						const kind: SwTaskDefinition = {
-							type: 'sw',
-							task: taskName
-						};
-						const bname = stsh + "_" + configuration[1];
-						var aname = action;
-						if (action === runExplan) {
-							aname = "build -e";
-						}
-						var cmd = "sw " + aname +
-							" --compiler " + compiler +
-							" -" + stsh +
-							" --config " + configuration[1] +
-							" --config-name " + bname + ""
-							;
-						if (action === "generate") {
-							cmd += " -g pcompdb";
-						}
-						if (action === "generate" || action === "configure" || action === runExplan) {
-							cmd += " --build-name " + bname;
-						}
-						const task = new vscode.Task(kind, workspaceFolder, taskName, 'sw', new vscode.ShellExecution(cmd), "$gcc");
-						result.push(task);
-						task.group = vscode.TaskGroup.Build;
-						if (action === "test") {
-							task.group = vscode.TaskGroup.Test;
+						for (const buildCurrentFile of addBuildCurrentFileOption) {
+							var taskName = action + " " + compiler + " " + stsh + " " + configuration[0];
+							if (action === runExplan) {
+								taskName += " (call after configure)";
+							}
+							if (buildCurrentFile) {
+								taskName += " (build current file)";
+							}
+							const kind: SwTaskDefinition = {
+								type: 'sw',
+								task: taskName,
+								action: action,
+								compiler: compiler,
+								configuration: configuration[0],
+								libraries: stsh,
+								buildCurrentFile: buildCurrentFile
+							};
+							const bname = compiler + "_" + stsh + "_" + configuration[1];
+							var aname = action;
+							if (action === runExplan) {
+								aname = "build -e";
+							}
+							var cmd = "sw " + aname +
+								" --compiler " + compiler +
+								" -" + stsh +
+								" --config " + configuration[1] +
+								" --config-name " + bname + ""
+								;
+							if (action === "generate") {
+								cmd += " -g compdb --local-targets-only --compdb-symlink";
+							}
+							if (action === "generate" || action === "configure" || action === runExplan) {
+								cmd += " --build-name " + bname;
+							}
+							if (buildCurrentFile) {
+								cmd += " --file \"${file}\"";
+							}
+							const task = new vscode.Task(kind, workspaceFolder, taskName, 'sw', new vscode.ShellExecution(cmd), "$gcc");
+							result.push(task);
+							task.group = vscode.TaskGroup.Build;
+							if (action === "test") {
+								task.group = vscode.TaskGroup.Test;
+							}
+							if (action === "build") {
+								build_cmd = cmd;
+							}
+							if (action === "generate") {
+								generate_cmd = cmd;
+							}
+							if (action === "configure") {
+								configure_cmd = cmd;
+							}
 						}
 					}
+
+					const taskName2 = "prepare for vs code" + " " + compiler + " " + stsh + " " + configuration[0];
+					const action = "prepare_for_vs_code";
+					const kind: SwTaskDefinition = {
+						type: 'sw',
+						task: taskName2,
+						action: action,
+						compiler: compiler,
+						configuration: configuration[0],
+						libraries: stsh,
+						buildCurrentFile: false
+					};
+					const task = new vscode.Task(kind, workspaceFolder, taskName2, 'sw',
+						new vscode.ShellExecution(build_cmd + " && " + generate_cmd + " && " + configure_cmd), "$gcc");
+					result.push(task);
+					task.group = vscode.TaskGroup.Build;
 				}
 			}
 		}
